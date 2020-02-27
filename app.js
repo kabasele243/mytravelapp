4f29a9aec533d34cca40753009196d694b0f7dd4
kabasele243 <fkabasele7@gmail.com>
2020-02-27 08:38:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const fs = require('fs')
+const app = express()
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+
+app.use(express.json())
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+          status: "success",
+          results: tours.length,
+          data: {
+              tours
+          }
+      })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id : newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status:'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
+const port = 4500;
+app.listen(port, () => {
+    console.log(`Server is on port ${port}`)
+})

ad7ac1d3a59e6a138e56844d8f7775e0a3b71d18
kabasele243 <fkabasele7@gmail.com>
2020-02-27 10:29:27 -0500

    Add User route

--- app.js
+++ app.js
@@ -98,24 +98,66 @@ const deleteTour =  (req, res) => {
     })
 }
 
-// app.get('/api/v1/tours', getAllTours)
-// app.get('/api/v1/tours/:id', getTour)
-// app.post('/api/v1/tours', createTour)
-// app.patch('/api/v1/tours', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
 
-app
+const getAllUsers = ((req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+})
+
+const createUser = ((req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+})
+
+const getUser = ((req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+})
+
+const updateUser = ((req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+})
+
+const deleteUser = ((req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    })
+})
+
+const tourRouter = express.Router();
+
+tourRouter
   .route('/api/v1/tours')
   .get(getAllTours)
   .post(createTour)
 
-app
+tourRouter
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour)
 
 
+app
+  .route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+
+app
+  .route('/ap/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser)
 
 const port = 4500;
 app.listen(port, () => {

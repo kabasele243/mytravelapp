3a20d972f52001836977a2bcf7daa4da46347a27
kabasele243 <fkabasele7@gmail.com>
2020-03-03 14:23:55 -0500

    Create tour with mongoose and MongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,25 +1,6 @@
 const Tour = require('./../models/tourModel')
 
 
-// exports.checkID = (req, res, next, val) => { 
-// if (req.params.id * 1 > tours.length) {
-//           return res.status(404).json({
-//             status: 'fail',
-//             message: 'Invalid ID'
-//           });
-//         }
-//     next();
-// };
-
-exports.checkBody = (req, res, next) => {
-if( !req.body.name || !req.body.price){
-    return res.status(400).json({
-    status: "fail",
-    message : "Missing name or price"})
-}
-next();
-}
-
 exports.getAllTours = (req, res) => {
     // res.status(200).json({
     //       status: "success",
@@ -45,19 +26,23 @@ exports.getTour = (req, res) => {
     //   })
 }
 
-exports.createTour = (req, res) => {
-    // const newId = tours[tours.length - 1].id + 1;
-    // const newTour = Object.assign({ id : newId}, req.body);
+exports.createTour = async (req, res) => {
+try {
+ const newTour = await Tour.create(req.body);
 
-    // tours.push(newTour);
-    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    //     res.status(201).json({
-    //         status:'success',
-    //         data: {
-    //             tour: newTour
-    //         }
-    //     })
-    // })
+ res.status(201).json({
+     status: 'success',
+     data: {
+         tour: newTour
+     }
+
+ });
+    } catch(err) {
+     res.status(400).json({
+         status: 'fail',
+         message: err
+     })
+ }
 }
 
 exports.updateTour = (req, res) => {

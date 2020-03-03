043612b8ae0a2fa436f3925d2b038ff4358e1b37
kabasele243 <fkabasele7@gmail.com>
2020-03-03 13:08:11 -0500

    connect our application with mongoDB through mongoose

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const express = require('express');
-
-const app = express();
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes')
+const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('dev'));
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware!');
-    next()
-})
+  console.log('Hello from the middleware 👋');
+  next();
+});
 
-app.use((req,res,next) => {
-    req.requestTime = new Date().toISOString();
-    next()
-})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
+// 3) ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;

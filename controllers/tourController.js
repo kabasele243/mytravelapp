037494d14128655e386346126b8242d43e9ad5bc
kabasele243 <fkabasele7@gmail.com>
2020-03-19 14:57:00 -0400

    last fix for our aliasing querying

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,10 @@
 const Tour = require('./../models/tourModel')
 
 exports.aliasTopTours = (req, res, next) => {
-
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage, price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
 };
 
 exports.getAllTours = async (req, res) => {

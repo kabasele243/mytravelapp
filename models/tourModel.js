ec320185666027698adbf67ddb23130f476623c3
kabasele243 <fkabasele7@gmail.com>
2020-03-20 15:56:12 -0400

    Using virtual properties with mongoose

--- models/tourModel.js
+++ models/tourModel.js
@@ -50,7 +50,17 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date]
+}, {
+    toJSON: {
+        virtuals: true
+    },
+    toObject: { virtuals: true}
 });
+
+tourSchema.virtual('durationWeeks').get(function() {
+    return this.duration / 7
+})
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 

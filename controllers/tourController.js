0d001df55a3a6d59bed4ab37d6ee42a06d43fd38
kabasele243 <fkabasele7@gmail.com>
2020-03-20 14:42:26 -0400

    refactoring api features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require('./../models/tourModel')
-
+const APIFeatures = require('./../utils/apiFeatures')
 exports.aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
     req.query.sort = '-ratingsAverage, price';
@@ -7,64 +7,28 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 };
 
+
+
 exports.getAllTours = async (req, res) => {
 try {
 
-    //BUILD QUERY
-    //1a. Filtering
-
-    const queryObj = { ...req.query}
-    const excludedFields = ['page', 'sort', 'limit', 'fields']
-    excludedFields.forEach(el => delete queryObj[el])
-
-    //1b. Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //2. Sorting
-    if(req.query.sort) {
-        const sortBy = req.query.sort.split(',').join(' ');
-        query = query.sort(sortBy)
-    } else {
-        query = query.sort('-createdAt');
-    }
-
-    //3. Field limiting
-    if(req.query.fields) {
-        const fields = req.query.fields.split(',').join(' ');
-        query = query.select(fields);
-    } else {
-        query = query.select('-__v');
-    }
-
-
-    //4. Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if(req.query.page) {
-        const numTours = await Tour.countDocuments();
-
-        if(skip >= numTours) throw new Error ('This page does not exist');
-    }
-
-    //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate()
+    const tours = await features.query;
 
     res.status(200).json({
           status: "success",
           results: tours.length,
           data: {
               tours
+              
           }
       })
     } catch(err) {
+      
         res.status(404).json({
             status: 'fail',
             message: "what???"

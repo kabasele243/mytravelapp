66dcf4af33f4689fe26e8ff695181e11eb5d904a
kabasele243 <fkabasele7@gmail.com>
2020-03-23 12:01:21 -0400

    adding aggregate middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -83,11 +83,18 @@ tourSchema.pre('save', function(next){
 //QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function(next) {
     this.find( { secretTour: { $ne: true } } )
+
+    this.start = Date.now();
     next();
 })
 
 tourSchema.post(/^find/, function(docs,next) {
-    
+    console.log(`Query took ${Date.now() - this.start } milliseconds`)
+    next();
+})
+
+tourSchema.pre('aggregate', function(next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true }}});
     next();
 })
 

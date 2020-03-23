42926b6a618949c8d32141df01fb609c5bce81aa
kabasele243 <fkabasele7@gmail.com>
2020-03-23 12:19:53 -0400

    using built-in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,7 +5,10 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true,
+        maxlength: [ 40, 'A tou name must have less or equal than 40 characters'],
+        minlenght: [ 10, 'A tour name must have more or equal then 10 characters']
     },
     slug: String,
     duration: {
@@ -18,11 +21,18 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: [ 'easy', 'medium', 'difficult'],
+            message: 'difficulty is either: easy, medium ,difficult'
+        },
+
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [ 5, 'Rating must be below 5.0']
     },
     ratingsQuantity:{
         type: Number,

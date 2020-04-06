d2074f06fbda6d245c225621a0f1692ffbe61b15
kabasele243 <fkabasele7@gmail.com>
2020-04-06 14:18:06 -0400

    handling error outside of express

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ mongoose
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! Shutting down...')
+  server.close(() => {
+    process.exit(1);
+  })
+})

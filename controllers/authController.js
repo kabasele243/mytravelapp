0cb69a31d08d61dbcf1c6d9205b91aaf05bbfdb0
kabasele243 <fkabasele7@gmail.com>
2020-04-07 13:12:28 -0400

    starting creating protecting route

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,17 @@ exports.login = catchAsync(async(req, res, next) => {
         token
     })
 });
+
+
+exports.protect = catchAsync(async (req, res, next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token =req.headers.authorization.split(' ')[1];
+    }
+
+    if(!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401))
+    }
+    next()
+})

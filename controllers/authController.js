7fa5b08528fda2b70e5ac8dce0da85bca98d414b
kabasele243 <fkabasele7@gmail.com>
2020-04-09 17:55:34 -0400

    Setting up user authorization, to restrict certains resources for certain user

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,15 +11,16 @@ const signToken = id => {
 }
 
 exports.signup = catchAsync(async(req, res, next) => {
+    
     const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     })
-
-    const token = signToken(user._id)
-
+    
+    const token = signToken(newUser._id)
+    
     res.status(201).json({
         status: 'success',
         token,
@@ -78,3 +79,14 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next()
 })
+
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new AppError('You do not have permission to perfom this action', 403))
+        }
+    }
+
+    next();
+}

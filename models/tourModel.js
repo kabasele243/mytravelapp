32f226c107d21c33af13eda472e89e3cf8065e5b
kabasele243 <fkabasele7@gmail.com>
2020-04-13 17:31:29 -0400

    Virtual Populate: Tours and Reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -122,6 +122,13 @@ tourSchema.virtual('durationWeeks').get(function() {
 })
 
 
+tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id'
+})
+
+
 //DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next){
     this.slug = slugify(this.name, { lower: true});

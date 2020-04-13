6aac0077bd33f93a29d7dbd270cb54f2d81afd93
kabasele243 <fkabasele7@gmail.com>
2020-04-13 13:09:30 -0400

    Modelling Start locations

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-const validator = require('validator')
+// const validator = require('validator')
+const User = require('./userModel')
+
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -100,6 +102,7 @@ const tourSchema = new mongoose.Schema({
           day: Number
         }
       ],
+      guides: Array
 },
     
  {
@@ -117,6 +120,14 @@ tourSchema.virtual('durationWeeks').get(function() {
 //DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next){
     this.slug = slugify(this.name, { lower: true});
+    next();
+})
+
+tourSchema.pre('save', async function(next){
+    const guidesPromises = this.guides.map(async id => await User.findById(id))
+
+    this.guides = await Promise.all(guidesPromises);
+
     next();
 })
 

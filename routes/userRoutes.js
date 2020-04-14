c7326831309384595762c6cfc6e414415c807627
kabasele243 <fkabasele7@gmail.com>
2020-04-14 14:53:23 -0400

    adding get me endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,7 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.get('/me', authController.protect, userController.getMe, userController.getUser)
 
 router.patch('/updateMyPassword',  
         authController.protect, 

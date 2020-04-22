a7bf94c3c58745ac628e0cdcbf3735817c75f001
Kabasele243 <kabasele467@gmail.com>
2020-04-22 13:22:18 -0400

    Creating our base template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -12,6 +13,13 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const app = express();
+
+app.set('view engine', 'pug')
+
+app.set('views', path.join(__dirname, 'views'))
+
+//Serving  static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Set security HTTP headers
 app.use(helmet())
@@ -52,8 +60,7 @@ app.use(hpp({
   ]
 }));
 
-//Serving  static files
-app.use(express.static(`${__dirname}/public`));
+
 
 
 //Test Middleware
@@ -63,6 +70,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base')
+})
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);

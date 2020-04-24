c9327cf7bd0d740d9e0b362ded5a2ea34ea6063f
Kabasele243 <kabasele467@gmail.com>
2020-04-24 15:45:38 -0400

    logging out users

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8465,7 +8465,7 @@ var login = /*#__PURE__*/function () {
             _context.next = 3;
             return (0, _axios.default)({
               method: 'POST',
-              url: 'http://127.0.0.1:3000/api/v1/users/login',
+              url: 'http://127.0.0.1:9000/api/v1/users/login',
               data: {
                 email: email,
                 password: password
@@ -8516,7 +8516,7 @@ var logout = /*#__PURE__*/function () {
             _context2.next = 3;
             return (0, _axios.default)({
               method: 'GET',
-              url: 'http://127.0.0.1:3000/api/v1/users/logout'
+              url: 'http://127.0.0.1:9000/api/v1/users/logout'
             });
 
           case 3:
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });

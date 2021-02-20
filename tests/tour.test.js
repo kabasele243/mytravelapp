0e5f43aef74b2c12f81289d1cb6f7fa88f8b09db
Franck Kabasele <kabasele467@gmail.com>
2021-02-19 22:12:09 -0500

    Update DB connection

--- tests/tour.test.js
+++ tests/tour.test.js
@@ -2,18 +2,24 @@ const app = require('../app')
 const supertest = require('supertest')
 const request = supertest(app)
 const mongoose = require("mongoose")
+const {setupDB, teardownDB} = require('./utils/db')
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '../config.env' });
 
 
 beforeEach((done) => {
+	// setupDB()
 	mongoose.connect(
-		"mongodb+srv://Franck:NWXdclrJB2xpFGT2@cluster0-8taph.mongodb.net/test?retryWrites=true&w=majority",
-		{ useNewUrlParser: true,useUnifiedTopology: true },
+		process.env.DATABASEDEV,
+		{ useNewUrlParser: true, useUnifiedTopology: true },
 		() => done()
 	)
 })
 
 afterEach((done) => {
-	mongoose.connection.db.dropDatabase(() => {
+	// teardownDB()
+	 mongoose.connection.db.dropDatabase(() => {
 		mongoose.connection.close(() => done())
 	})
 })
@@ -22,4 +28,4 @@ test('Should Get All the tours', async (done) => {
     const res = await request.get('/api/v1/tours') 
     expect(res.status).toBe(200)
     done()
-})
+}, 10000)
